65064a365b2fc021d190db25de8a6a071c3e7a6c
Pierre Dal-Pra <dalpra.pierre@gmail.com>
2018-01-24 21:06:54 +0100

    wip

--- modules/kernsec.nix
+++ modules/kernsec.nix
@@ -2,18 +2,18 @@
 
 {
     nixpkgs.config.packageOverrides = pkgs: {
-         linux_4_14 = pkgs.linux_4_14.override {
-                extraConfig = ''
-                  DEBUG_WX             y
-                  KEXEC                n
-                  IO_STRICT_DEVMEM     y
-                  PROC_KCORE           n
-                  ACPI_CUSTOM_METHOD   n
-                  FORTIFY_SOURCE       y
-                  REFCOUNT_FULL        y
-                  SLAB_FREELIST_RANDOM y
-                  HARDENED_USERCOPY    y
-                '';
-            };
+        linux_4_14 = pkgs.linux_4_14.override {
+            extraConfig = ''
+              DEBUG_WX             y
+              KEXEC                n
+              IO_STRICT_DEVMEM     y
+              PROC_KCORE           n
+              ACPI_CUSTOM_METHOD   n
+              FORTIFY_SOURCE       y
+              REFCOUNT_FULL        y
+              SLAB_FREELIST_RANDOM y
+              HARDENED_USERCOPY    y
+            '';
+        };
     };
 }
