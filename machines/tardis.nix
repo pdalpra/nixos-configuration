7060519ed5b48cd06bb1ed2b900af79b55d4e2a6
Pierre Dal-Pra <dalpra.pierre@gmail.com>
2018-03-10 19:39:56 +0100

    Add tardis filesystems configuration

--- machines/tardis.nix
+++ machines/tardis.nix
@@ -9,6 +9,7 @@
 
     boot.initrd.luks = {
         mitigateDMAAttacks = true;
+        yubikeySupport     = true;
         devices.lvm = {
             device        = "/dev/nvme0n1p2";
             allowDiscards = true;
@@ -22,4 +23,36 @@
     };
 
     time.timeZone = "Europe/Paris";
+
+    fileSystems = {
+        "/boot" = {
+            device  = "/dev/nvme0n1p1";
+            fsType  = "vfat";
+            options = [
+                "rw" "noatime" "nodiratime" "data=orderedrw" "fmask=0022" "dmask=0022"
+                "codepage=437" "iocharset=iso8859-1" "shortname=mixed" "errors=remount-ro"
+            ];
+        };
+        "/" = {
+            device  = "/dev/mapper/lvm-root";
+            fsType  = "ext4";
+            options = ["rw" "noatime" "nodiratime" "data=ordered"];
+        };
+        "/home" = {
+            device  = "/dev/mapper/lvm-home";
+            fsType  = "ext4";
+            options = ["rw" "noatime" "nodiratime" "data=ordered" "nofail"];
+        };
+        "/var/lib/docker" = {
+            device  = "/dev/mapper/lvm-docker";
+            fsType  = "ext4";
+            options = ["rw" "noatime" "nodiratime" "data=ordered" "nofail"];
+        };
+    };
+
+    swapDevices = [
+        {
+            device = " /dev/mapper/lvm-swap";
+        }
+    ];
 }
